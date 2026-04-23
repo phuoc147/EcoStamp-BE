b4ad5d193cc5deadbe403e702b13ef642f413976
phuoc147 <phuoc.hatruong@hcmut.edu.vn>
2026-04-23 19:39:13 +0700

    update: modify cross-origin

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", env.feUrl as string],
     credentials: true,
   }),
 );
--- src/config/index.ts
+++ src/config/index.ts
@@ -6,5 +6,6 @@ export const env = {
   nodeEnv: process.env.NODE_ENV ?? "development",
   port: Number(process.env.PORT ?? 4000),
   databaseUrl: process.env.DATABASE_URL,
+  feUrl: process.env.FE_URL,
 };
 
